767a54a04a94fab6e3cde6b10755871c233a9b6e
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2022-10-06 09:05:29 +0200

    update

---
+++ input.tf
@@ -0,0 +1,8 @@
+variable "additional_tags" {
+  default = {}
+  type    = map(string)
+}
+
+variable "subnet_id" {
+  type = string
+}
---
+++ main.tf
@@ -0,0 +1,40 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "this" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  network_interface {
+    network_interface_id = aws_network_interface.this.id
+    device_index         = 0
+  }
+  tags = merge(
+    var.additional_tags,
+    {
+      created-by = "iac-tf"
+    },
+  )
+}
+
+resource "aws_network_interface" "this" {
+  subnet_id   = var.subnet_id
+  tags = merge(
+    var.additional_tags,
+    {
+      created-by = "iac-tf"
+    },
+  )
+}
